a26dee1fbbbc5173dfe01ba4c4b75a8bdf0812ab
Zafar <zafar142007@gmail.com>
2016-11-18 09:34:21 +0530

    Update install-opencv.sh

--- install-opencv.sh
+++ install-opencv.sh
@@ -36,27 +36,40 @@ sudo apt install libgstreamer-plugins-base1.0-dev
 
 # INSTALL THE LIBRARY (YOU CAN CHANGE '3.1.0' FOR THE LAST STABLE VERSION)
 
-sudo apt-get install -y unzip wget
-wget https://github.com/opencv/opencv/archive/3.1.0.zip
-unzip 3.1.0.zip
-rm 3.1.0.zip
-mv opencv-3.1.0 OpenCV
+sudo apt install git
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout d1bbc0b6e6c0c5115e54aa6d9f32a1d332a9538c
 
-wget https://github.com/opencv/opencv_contrib/archive/3.1.0.zip
-unzip 3.1.0.zip
-mv opencv_contrib-3.1.0/ OpenCV_contrib
-rm -rf 3.1.0.zip 
+cd ..
 
-cd OpenCV
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout b7dcf141507edbe544e75820c76769a7769223ac
+cd ..
+
+#sudo apt-get install -y unzip wget
+#wget https://github.com/opencv/opencv/archive/3.1.0.zip
+#unzip 3.1.0.zip
+#rm 3.1.0.zip
+#mv opencv-3.1.0 OpenCV
+
+#wget https://github.com/opencv/opencv_contrib/archive/3.1.0.zip
+#unzip 3.1.0.zip
+#mv opencv_contrib-3.1.0/ OpenCV_contrib
+#rm -rf 3.1.0.zip 
+
+cd opencv
+
 #hdf5 file not found bugfix
-echo "find_package(HDF5)" >> modules/python/common.cmake
-echo "include_directories(${HDF5_INCLUDE_DIRS})" >> modules/python/common.cmake
+#echo "find_package(HDF5)" >> modules/python/common.cmake
+#echo "include_directories(${HDF5_INCLUDE_DIRS})" >> modules/python/common.cmake
 sudo apt-get install libhdf5-serial-dev hdf5-tools
 
 mkdir build
 cd build
-cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DWITH_OPENGL=ON -DWITH_QT=ON -DBUILD_EXAMPLES=ON -DOPENCV_EXTRA_MODULES_PATH=../../OpenCV_contrib/modules ..
-make -j4
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DWITH_OPENGL=ON -DWITH_QT=ON -DBUILD_EXAMPLES=ON -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+make -j8 > /opt/logs/make.log &
 sudo make install
 sudo ldconfig
 
